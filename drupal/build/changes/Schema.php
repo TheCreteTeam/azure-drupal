62419d55da39d1fba75a8051508ac1683ef509d0
Konstantinos Venizelos <kvenizelos@live.com>
2024-07-04 16:54:18 +0300

    Fix issue: https://www.drupal.org/project/sqlsrv/issues/2857769

--- drupal/build/changes/Schema.php
+++ drupal/build/changes/Schema.php
@@ -1643,18 +1643,14 @@ EOF
    */
   public function getSizeInfo() {
     $sql = <<< EOF
-      SELECT
-    DB_NAME(db.database_id) DatabaseName,
-    (CAST(mfrows.RowSize AS FLOAT)*8)/1024 RowSizeMB,
-    (CAST(mflog.LogSize AS FLOAT)*8)/1024 LogSizeMB,
-    (CAST(mfstream.StreamSize AS FLOAT)*8)/1024 StreamSizeMB,
-    (CAST(mftext.TextIndexSize AS FLOAT)*8)/1024 TextIndexSizeMB
-FROM sys.databases db
-    LEFT JOIN (SELECT database_id, SUM(size) RowSize FROM sys.master_files WHERE type = 0 GROUP BY database_id, type) mfrows ON mfrows.database_id = db.database_id
-    LEFT JOIN (SELECT database_id, SUM(size) LogSize FROM sys.master_files WHERE type = 1 GROUP BY database_id, type) mflog ON mflog.database_id = db.database_id
-    LEFT JOIN (SELECT database_id, SUM(size) StreamSize FROM sys.master_files WHERE type = 2 GROUP BY database_id, type) mfstream ON mfstream.database_id = db.database_id
-    LEFT JOIN (SELECT database_id, SUM(size) TextIndexSize FROM sys.master_files WHERE type = 4 GROUP BY database_id, type) mftext ON mftext.database_id = db.database_id
-    WHERE DB_NAME(db.database_id) = :database
+    SELECT
+        db.name DatabaseName,
+        (CAST((SELECT  SUM(size) RowSize FROM sys.database_files WHERE type = 0 ) AS FLOAT)*8)/1024 RowSizeMB,
+        (CAST((SELECT SUM(size) LogSize FROM sys.database_files WHERE type = 1 ) AS FLOAT)*8)/1024 LogSizeMB,
+        (CAST((SELECT SUM(size) StreamSize FROM sys.database_files WHERE type = 2 ) AS FLOAT)*8)/1024 StreamSizeMB,
+        (CAST((SELECT SUM(size) TextIndexSize FROM sys.database_files WHERE type = 4 ) AS FLOAT)*8)/1024 TextIndexSizeMB
+    FROM sys.databases db 
+        WHERE db.name = :database
 EOF;
     // Database is defaulted from active connection.
     $options = $this->connection->getConnectionOptions();
